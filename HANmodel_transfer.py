e5999ef576c7e7a4de427b30e1fe5e89c30bc783
VaaaarianZhou <varianz@andrew.cmu.edu>
2024-12-04 11:01:05 -0500

    Add files via upload

---
+++ HANmodel_transfer.py
@@ -0,0 +1,192 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+
+from torch_geometric.data import HeteroData
+from torch_geometric.loader import HGTLoader
+from torch_geometric.nn import HANConv
+from tqdm import tqdm
+import torch.nn.functional as F
+from utils import load_hubmap_data
+
+# Custom module functions (ensure these are properly defined in your 'data_processing' module)
+from data_processing import (
+    load_csv,
+    encode_cell_types,
+    construct_similarity_adjacency,
+    construct_spatial_adjacency
+)
+
+ANNOTATED_DATA = 'data/B004_training_dryad.csv'
+UNANNOTATED_DATA = 'data/B0056_unnanotated_dryad.csv'
+dist_threshold = 30
+neighborhood_size_threshold = 10
+sample_rate = .05
+
+# Set device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Load train and test data
+train_X, train_y, test_X, labeled_spatial_edges, unlabeled_spatial_edges, labeled_similarity_edges, unlabeled_similarity_edges, inverse_dict = load_hubmap_data(ANNOTATED_DATA, UNANNOTATED_DATA, dist_threshold, neighborhood_size_threshold, sample_rate)
+
+# Convert them into torch.tensor
+train_X = torch.tensor(train_X, device=device, dtype = torch.float)
+train_y = torch.tensor(train_y, device=device, dtype = torch.long)
+test_X = torch.tensor(test_X, device=device, dtype = torch.float)
+labeled_spatial_edges = torch.tensor(labeled_spatial_edges, device=device, dtype = torch.long).T
+unlabeled_spatial_edges = torch.tensor(unlabeled_spatial_edges, device=device, dtype = torch.long).T
+labelled_similarity_edges = torch.tensor(labeled_similarity_edges, device=device, dtype = torch.long).T
+unlabelled_similarity_edges = torch.tensor(unlabeled_similarity_edges, device=device, dtype = torch.long).T
+
+
+# Build the heterogeneous graph
+train_edge_index_dict = {
+    ('cell', 'spatially_close_to', 'cell'): {'edge_index': labeled_spatial_edges},
+    ('cell', 'similar_to', 'cell'): {'edge_index' : labelled_similarity_edges}
+}
+
+test_edge_index_dict = {
+    ('cell', 'spatially_close_to', 'cell'): {'edge_index': unlabeled_spatial_edges},
+    ('cell', 'similar_to', 'cell'): {'edge_index' :unlabelled_similarity_edges}
+}
+
+# Define training dataset
+train_hetero_data = HeteroData(train_edge_index_dict)
+train_hetero_data['cell'].x = torch.tensor(train_X, device=device, dtype=torch.float)
+train_hetero_data['cell'].y = torch.tensor(train_y, device=device, dtype=torch.long)
+
+# Define test dataset
+test_hetero_data = HeteroData(test_edge_index_dict)
+test_hetero_data['cell'].x = torch.tensor(train_X, device=device, dtype=torch.float)
+
+
+''' Training HAN model'''
+# Assign train and validation masks
+num_nodes = train_hetero_data['cell'].num_nodes
+train_ratio = 0.8
+num_train = int(num_nodes * train_ratio)
+
+perm = torch.randperm(num_nodes)
+train_idx = perm[:num_train]
+test_idx = perm[num_train:]
+
+train_hetero_data['cell'].train_mask = torch.zeros(num_nodes, dtype=torch.bool)
+train_hetero_data['cell'].train_mask[train_idx] = True
+
+train_hetero_data['cell'].test_mask = torch.zeros(num_nodes, dtype=torch.bool)
+train_hetero_data['cell'].test_mask[test_idx] = True
+
+train_loader = HGTLoader(train_hetero_data, num_samples=[64], shuffle=True, batch_size = 128,
+                             input_nodes=('cell', train_hetero_data['cell'].train_mask))
+val_loader = HGTLoader(train_hetero_data, num_samples=[64], batch_size = 128,
+                           input_nodes=('cell', train_hetero_data['cell'].test_mask))
+
+# Get metadata
+metadata = train_hetero_data.metadata()
+
+# Define the HAN model
+class HAN(torch.nn.Module):
+    def __init__(self, metadata, in_channels, out_channels, hidden_channels=128, heads=2):
+        super().__init__()
+        self.conv1 = HANConv(in_channels, hidden_channels, heads=heads, dropout=0.6, metadata=metadata)
+        self.conv2 = HANConv(hidden_channels, hidden_channels, heads=heads, dropout=0.6, metadata=metadata)
+        self.lin = nn.Linear(hidden_channels, out_channels)
+
+    def forward(self, x_dict, edge_index_dict):
+        out = self.conv1(x_dict, edge_index_dict)
+        out = {key: x.relu() for key, x in out.items()}
+        out = self.conv2(out , edge_index_dict)
+        out = {key: x.relu() for key, x in out.items()}
+        out = self.lin(out['cell'])
+        return out
+
+in_channels = train_hetero_data['cell'].x.size(1)
+num_classes = train_hetero_data['cell'].y.max().item() + 1
+
+model = HAN(metadata, in_channels, num_classes, hidden_channels=128, heads=2).to(device)
+
+# Define optimizer and loss function
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0001, weight_decay=0.001)
+
+# Training loop
+train_losses = []
+test_accuracies = []
+num_epochs = 200
+
+# print(hetero_data.edge_index_dict)
+
+for epoch in tqdm(range(1, num_epochs + 1), desc='Epoch'):
+    model.train()
+    total_examples = total_loss = 0
+    for batch in train_loader:
+        optimizer.zero_grad()
+        batch = batch.to(device, 'edge_index')
+        batch_size = batch['cell'].batch_size
+        out = model(batch.x_dict, batch.edge_index_dict)[:batch_size]
+
+        loss = F.cross_entropy(out, batch['cell'].y[:batch_size])
+        loss.backward()
+        optimizer.step()
+        total_examples += batch_size
+        total_loss += float(loss.item()) * batch_size
+    train_losses.append(total_loss)
+
+
+    # Validation
+    model.eval()
+    total_examples = total_correct = 0
+    for batch in val_loader:
+        batch = batch.to(device, 'edge_index')
+        batch_size = batch['cell'].batch_size
+        out = model(batch.x_dict, batch.edge_index_dict)[:batch_size]
+        pred = out.argmax(dim=-1)
+
+        total_examples += batch_size
+        total_correct += int((pred == batch['cell'].y[:batch_size]).sum())
+
+    val_acc = total_correct / total_examples
+    test_accuracies.append(val_acc)
+
+print(f'Epoch: {epoch}, Loss: {loss.item():.4f}, Test Accuracy: {val_acc:.4f}')
+
+# Plot the training loss curve
+plt.figure(figsize=(10, 5))
+plt.plot(range(1, num_epochs + 1), train_losses, label='Training Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.title('Training Loss Curve')
+plt.legend()
+plt.show()
+
+# Plot the test accuracy over epochs
+plt.figure(figsize=(10, 5))
+plt.plot(range(1, num_epochs + 1), test_accuracies, label='Test Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.title('Test Accuracy over Epochs')
+plt.legend()
+plt.show()
+
+# Evaluate the model
+model.eval()
+with torch.no_grad():
+    out = model(test_hetero_data.x_dict, test_hetero_data.edge_index_dict)
+    pred = out.argmax(dim=1)
+    y_pred = pred[test_hetero_data['cell']].cpu().numpy()
+    # y_true = test_hetero_data['cell'].y[test_hetero_data['cell'].test_mask].cpu().numpy()
+
+# Compute and plot the confusion matrix
+# cm = confusion_matrix(y_true, y_pred)
+# plt.figure(figsize=(12, 10))
+# disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+# disp.plot(cmap=plt.cm.Blues)
+# plt.title('Confusion Matrix')
+# plt.show()
+#
+# # Print classification report
+# print(classification_report(y_true, y_pred))
---
+++ utils.py
@@ -0,0 +1,119 @@
+import torch
+import numpy as np
+from builtins import range
+from torch_geometric.data import InMemoryDataset, Data
+from sklearn.metrics import pairwise_distances
+import pandas as pd
+from sklearn.preprocessing import normalize, LabelEncoder
+
+genes = ['MUC2', 'SOX9', 'MUC1', 'CD31', 'Synapto', 'CD49f',
+       'CD15', 'CHGA', 'CDX2', 'ITLN1', 'CD4', 'CD127', 'Vimentin', 'HLADR',
+       'CD8', 'CD11c', 'CD44', 'CD16', 'BCL2', 'CD3', 'CD123', 'CD38', 'CD90',
+       'aSMA', 'CD21', 'NKG2D', 'CD66', 'CD57', 'CD206', 'CD68', 'CD34',
+       'aDef5', 'CD7', 'CD36', 'CD138', 'CD45RO', 'Cytokeratin', 'CK7',
+       'CD117', 'CD19', 'Podoplanin', 'CD45', 'CD56', 'CD69', 'Ki67', 'CD49a',
+       'CD163', 'CD161']
+
+def get_hubmap_edge_index(X, pos, regions, distance_thres, neighborhood_size_thres):
+    # construct edge indexes when there is region information
+    spatial_edge_list = []
+
+    # For similarity, src and dst
+    src = []
+    dst = []
+    regions_unique = np.unique(regions)
+    for reg in regions_unique:
+        # Build spatial edges
+        locs = np.where(regions == reg)[0]
+        pos_region = pos[locs, :]
+        dists = pairwise_distances(pos_region)
+        dists_mask = dists < distance_thres
+        np.fill_diagonal(dists_mask, 0)
+        region_edge_list = np.transpose(np.nonzero(dists_mask)).tolist()
+        for (i, j) in region_edge_list:
+            spatial_edge_list.append([locs[i], locs[j]])
+        # Build molecular similarity edges
+
+        X_region = X[locs, :]
+        for idx, row in enumerate(X_region):
+            scores = row @ X_region.T
+            neighbors = scores.argsort()[-neighborhood_size_thres:]
+            # src.append(np.full((neighborhood_size_thres,), idx))
+            src.extend([idx]*neighborhood_size_thres)
+            dst.extend(neighbors)
+        # similarity_edge_index = np.stack([np.concatenate(src), np.concatenate(dst)])
+        # edge_weights = torch.concat(edge_weights)
+
+    similarity_edge_list = [[u,v] for (u,v) in zip(src, dst)]
+    return spatial_edge_list, similarity_edge_list
+
+def get_tonsilbe_edge_index(pos, distance_thres):
+    # construct edge indexes in one region
+    edge_list = []
+    dists = pairwise_distances(pos)
+    dists_mask = dists < distance_thres
+    np.fill_diagonal(dists_mask, 0)
+    edge_list = np.transpose(np.nonzero(dists_mask)).tolist()
+    return edge_list
+
+def load_hubmap_data(labeled_file, unlabeled_file, distance_thres, neighborhood_size_thres, sample_rate):
+    train_df = pd.read_csv(labeled_file)
+    test_df = pd.read_csv(unlabeled_file)
+    train_df = train_df.sample(n=round(sample_rate*len(train_df)), random_state=1)
+    test_df = test_df.sample(n=round(sample_rate*len(test_df)), random_state=1)
+    train_df = train_df.loc[np.logical_and(train_df['tissue'] == 'CL', train_df['donor'] == 'B004')]
+    test_df = test_df.loc[np.logical_and(test_df['tissue'] == 'CL', test_df['donor'] == 'B005')]
+    train_X = train_df.iloc[:, 1:49].to_numpy() # node features, indexes depend on specific datasets
+    train_X = normalize(train_X)
+    test_X = test_df.iloc[:, 1:49].to_numpy()
+    test_X = normalize(test_X)
+    labeled_pos = train_df.iloc[:, -6:-4].values # x,y coordinates, indexes depend on specific datasets
+    unlabeled_pos = test_df.iloc[:, -5:-3].values
+    labeled_regions = train_df['unique_region']
+    unlabeled_regions = test_df['unique_region']
+    train_y = train_df['cell_type_A'] # class information
+    cell_types = np.sort(list(set(train_df['cell_type_A'].values))).tolist()
+    # we here map class in texts to categorical numbers and also save an inverse_dict to map the numbers back to texts
+    cell_type_dict = {}
+    inverse_dict = {}
+    for i, cell_type in enumerate(cell_types):
+        cell_type_dict[cell_type] = i
+        inverse_dict[i] = cell_type
+    train_y = np.array([cell_type_dict[x] for x in train_y])
+    labeled_spatial_edges, labeled_similarity_edges = get_hubmap_edge_index(train_X, labeled_pos, labeled_regions, distance_thres, neighborhood_size_thres)
+    unlabeled_spatial_edges, unlabeled_similarity_edges = get_hubmap_edge_index(test_X, unlabeled_pos, unlabeled_regions, distance_thres, neighborhood_size_thres)
+    return train_X, train_y, test_X, labeled_spatial_edges, unlabeled_spatial_edges, labeled_similarity_edges, unlabeled_similarity_edges, inverse_dict
+def load_tonsilbe_data(filename, distance_thres, sample_rate):
+    df = pd.read_csv(filename)
+    train_df = df.loc[df['sample_name'] == 'tonsil']
+    train_df = train_df.sample(n=round(sample_rate*len(train_df)), random_state=1)
+    test_df = df.loc[df['sample_name'] == 'Barretts Esophagus']
+    train_X = train_df.iloc[:, 1:-4].values
+    test_X = test_df.iloc[:, 1:-4].values
+    train_y = train_df['cell_type'].str.lower()
+    labeled_pos = train_df.iloc[:, -4:-2].values
+    unlabeled_pos = test_df.iloc[:, -4:-2].values
+    cell_types = np.sort(list(set(train_y))).tolist()
+    cell_type_dict = {}
+    inverse_dict = {}
+    for i, cell_type in enumerate(cell_types):
+        cell_type_dict[cell_type] = i
+        inverse_dict[i] = cell_type
+    train_y = np.array([cell_type_dict[x] for x in train_y])
+    labeled_spatial_edges, labeled_similarity_edges = get_tonsilbe_edge_index(labeled_pos, distance_thres)
+    unlabeled_spatial_edges, unlabeled_similarity_edges = get_tonsilbe_edge_index(unlabeled_pos, distance_thres)
+    return train_X, train_y, test_X, labeled_spatial_edges, unlabeled_spatial_edges, labeled_similarity_edges, unlabeled_similarity_edges, inverse_dict
+
+class GraphDataset(InMemoryDataset):
+
+    def __init__(self, labeled_X, labeled_y, unlabeled_X, labeled_edges, unlabeled_edges, transform=None,):
+        self.root = '.'
+        super(GraphDataset, self).__init__(self.root, transform)
+        self.labeled_data = Data(x=torch.FloatTensor(labeled_X), edge_index=torch.LongTensor(labeled_edges).T, y=torch.LongTensor(labeled_y))
+        self.unlabeled_data = Data(x=torch.FloatTensor(unlabeled_X), edge_index=torch.LongTensor(unlabeled_edges).T)
+
+    def __len__(self):
+        return 2
+
+    def __getitem__(self, idx):
+        return self.labeled_data, self.unlabeled_data
