3362b4ca007ecabdd053cf617f89237e43426b34
VaaaarianZhou <varianz@andrew.cmu.edu>
2024-12-10 21:09:45 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -16,6 +16,11 @@ import pandas as pd
 from sklearn.preprocessing import normalize, LabelEncoder
 import umap
 import matplotlib.pyplot as plt
+import csv
+import os
+import torch
+import numpy as np
+
 
 
 genes = ['MUC2', 'SOX9', 'MUC1', 'CD31', 'Synapto', 'CD49f',
@@ -34,7 +39,7 @@ def get_hubmap_edge_index(X, pos, regions, distance_thres, neighborhood_size_thr
     src = []
     dst = []
     regions_unique = np.unique(regions)
-    index = np.arange(len(X.shape[0]))
+    index = np.arange(X.shape[0])
     for reg in regions_unique:
         # Build spatial edges
         locs = np.where(regions == reg)[0]
@@ -69,21 +74,22 @@ def get_tonsilbe_edge_index(pos, distance_thres):
     edge_list = np.transpose(np.nonzero(dists_mask)).tolist()
     return edge_list
 
-def load_hubmap_data(labeled_file, unlabeled_file, distance_thres, neighborhood_size_thres, sample_rate):
-    train_df = pd.read_csv(labeled_file)
-    test_df = pd.read_csv(unlabeled_file)
+# def load_hubmap_data(labeled_file, unlabeled_file, distance_thres, neighborhood_size_thres, sample_rate):
+
+def load_hubmap_data(train_df, distance_thres, neighborhood_size_thres, sample_rate):
+    # test_df = pd.read_csv(unlabeled_file)
     train_df = train_df.sample(n=round(sample_rate*len(train_df)), random_state=1)
-    test_df = test_df.sample(n=round(sample_rate*len(test_df)), random_state=1)
+    # test_df = test_df.sample(n=round(sample_rate*len(test_df)), random_state=1)
     train_df = train_df[(train_df['tissue'] == 'CL') & (train_df['donor'] == 'B004')]
-    test_df = test_df[(test_df['tissue'] == 'CL') & (test_df['donor'] == 'B005')]
+    # test_df = test_df[(test_df['tissue'] == 'CL') & (test_df['donor'] == 'B005')]
     train_X = train_df.iloc[:, 1:49].to_numpy() # node features, indexes depend on specific datasets
-    train_X = normalize(train_X)
-    test_X = test_df.iloc[:, 1:49].to_numpy()
-    test_X = normalize(test_X)
+    # train_X = normalize(train_X)
+    # test_X = test_df.iloc[:, 1:49].to_numpy()
+    # test_X = normalize(test_X)
     labeled_pos = train_df.iloc[:, -6:-4].values # x,y coordinates, indexes depend on specific datasets
-    unlabeled_pos = test_df.iloc[:, -5:-3].values
+    # unlabeled_pos = test_df.iloc[:, -5:-3].values
     labeled_regions = train_df['unique_region']
-    unlabeled_regions = test_df['unique_region']
+    # unlabeled_regions = test_df['unique_region']
     train_y = train_df['cell_type_A'] # class information
     cell_types = np.sort(list(set(train_df['cell_type_A'].values))).tolist()
     # we here map class in texts to categorical numbers and also save an inverse_dict to map the numbers back to texts
@@ -94,8 +100,11 @@ def load_hubmap_data(labeled_file, unlabeled_file, distance_thres, neighborhood_
         inverse_dict[i] = cell_type
     train_y = np.array([cell_type_dict[x] for x in train_y])
     labeled_spatial_edges, labeled_similarity_edges = get_hubmap_edge_index(train_X, labeled_pos, labeled_regions, distance_thres, neighborhood_size_thres)
-    unlabeled_spatial_edges, unlabeled_similarity_edges = get_hubmap_edge_index(test_X, unlabeled_pos, unlabeled_regions, distance_thres, neighborhood_size_thres)
-    return train_X, train_y, test_X, labeled_spatial_edges, unlabeled_spatial_edges, labeled_similarity_edges, unlabeled_similarity_edges, inverse_dict
+    # unlabeled_spatial_edges, unlabeled_similarity_edges = get_hubmap_edge_index(test_X, unlabeled_pos, unlabeled_regions, distance_thres, neighborhood_size_thres)
+    # return train_X, train_y, test_X, labeled_spatial_edges, unlabeled_spatial_edges, labeled_similarity_edges, unlabeled_similarity_edges, inverse_dict
+    return train_X, train_y, labeled_spatial_edges, labeled_similarity_edges, inverse_dict
+
+
 def load_tonsilbe_data(filename, distance_thres, sample_rate):
     df = pd.read_csv(filename)
     train_df = df.loc[df['sample_name'] == 'tonsil']
@@ -146,6 +155,62 @@ def visualize_predictions(X, predicted_labels, inverse_dict, save_location):
     plt.tight_layout()
     plt.savefig(save_location)
     # plt.show()
+
+def write_result_to_csv(result_path, model_name, threshold_distance, topk, val_loss, validation_acc):
+    with open(os.path.join(result_path, 'result.csv'), 'a', newline='') as csvfile:
+        fieldnames = ['Model', 'Threshold_Distance', 'TopK', 'Validation_Loss', 'Validation_Accuracy']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        writer.writerow({'Model': model_name, 'Threshold_Distance': threshold_distance, 'TopK': topk, 'Validation_Loss':val_loss, 'Validation_Accuracy': validation_acc})
+
+
+class EarlyStopping:
+    """Early stops the training if validation loss doesn't improve after a given patience."""
+    def __init__(self, patience=7, verbose=False, delta=0.0, path='checkpoint.pt'):
+        """
+        Args:
+            patience (int): How long to wait after last time validation loss improved.
+                            Default: 7
+            verbose (bool): If True, prints a message for each validation loss improvement.
+                            Default: False
+            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+                           Default: 0.0
+            path (str): Path for the checkpoint to be saved to.
+                        Default: 'checkpoint.pt'
+        """
+        self.patience = patience
+        self.verbose = verbose
+        self.delta = delta
+        self.path = path
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+
+    def __call__(self, val_loss, model):
+        score = -val_loss  # Since we want to minimize loss, a smaller val_loss is better. Hence, invert sign.
+
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            if self.verbose:
+                print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        """Saves model when validation loss decreases."""
+        if self.verbose:
+            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(model.state_dict(), self.path)
+        self.val_loss_min = val_loss
 
 
 class GraphDataset(InMemoryDataset):
